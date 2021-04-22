64c3db4163a1ed4deae5b4dca749bb9a87162bc5
MamtaNew <mamtanew@MamtaNew.local>
2021-04-23 00:58:47 +0530

    ios rgenerate

--- ios/FoodAppTests/FoodAppTests.m
+++ ios/FoodAppTests/FoodAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FeedAppTests : XCTestCase
+@interface FoodAppTests : XCTestCase
 
 @end
 
-@implementation FeedAppTests
+@implementation FoodAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
